cc88834159d7706d919df58d36994f6157abb62a
Christophe SAUVEUR <chsxf.pro@gmail.com>
2025-08-02 10:16:24 +0200

    🔧 Updated target platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CiderKit.Tween",
     platforms: [
-        .macOS(.v14), .iOS(.v12), .tvOS(.v12)
+        .macOS(.v14), .iOS(.v13), .macCatalyst(.v13), .tvOS(.v13), .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
