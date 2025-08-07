5e823a16500f67ed7da41abfb4b02a12d974fcf3
Christophe SAUVEUR <chsxf.pro@gmail.com>
2025-08-07 07:23:03 +0200

    ✨ Adding access to `TweenInstance` properties via the `Tween` struct

--- Sources/CiderKit.Tween/Tween.swift
+++ Sources/CiderKit.Tween/Tween.swift
@@ -4,6 +4,9 @@ public struct Tween<T: Sendable>: Sendable {
     internal let instance: TweenInstance
     internal let data: TweenData<T>
 
+    public var isRunning: Bool { get async { await instance.isRunning } }
+    public var isComplete: Bool { get async { await instance.isComplete } }
+
     internal init(_ instance: TweenInstance, _ data: TweenData<T>) {
         self.instance = instance
         self.data = data
