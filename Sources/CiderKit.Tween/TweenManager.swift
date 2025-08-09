af22b78592bdfe875704593dac906d510ef9f440
Christophe SAUVEUR <chsxf.pro@gmail.com>
2025-08-09 17:03:04 +0200

    📝 `TweenManager` documentation

--- Sources/CiderKit.Tween/TweenManager.swift
+++ Sources/CiderKit.Tween/TweenManager.swift
@@ -2,39 +2,60 @@
 import AppKit
 #endif
 
+/// Global actor responsible for managing automatic tweens
+///
+/// `TweenManager` relies on the `CADisplayLink` class to obtain animation timings based on the current display refresh rate.
+///
+/// > Warning: Unfortunately, `CADisplayLink` instances are obtained differently on macOS and the other platforms.
+/// Consequently, you will need to call one of the available `start` functions for the tweens to animate automatically.
 @globalActor
 public actor TweenManager: GlobalActor {
 
+    /// Accessor to the shared instance of the global actor
     public static let shared = TweenManager()
 
     private var runningTweenInstances = [TweenInstance]()
     private var displayLinkProxy: DisplayLinkProxy?
 
-    #if os(macOS)
+    /// Starts the `TweenManager` by getting a `CADisplayLink` instance from an `NSView`. Available on macOS only.
+    ///
+    /// - Parameter view: View from which to get the `CADisplayLink` instance
+    @available(macOS 14, *)
     public func startFrom(view: NSView) async {
+        #if os(macOS)
         guard displayLinkProxy == nil else {
             fatalError("TweenManager already initialized")
         }
 
         displayLinkProxy = await DisplayLinkProxy(view: view)
         startPollingTimeIntervals()
+        #endif
     }
 
+    /// Starts the `TweenManager` by getting a `CADisplayLink` instance from an `NSWindow`. Available on macOS only.
+    ///
+    /// - Parameter window: Window from which to get the `CADisplayLink` instance
+    @available(macOS 14, *)
     public func startFrom(window: NSWindow) async {
+        #if os(macOS)
         guard displayLinkProxy == nil else {
             fatalError("TweenManager already initialized")
         }
 
         displayLinkProxy = await DisplayLinkProxy(window: window)
         startPollingTimeIntervals()
+        #endif
     }
-    #else
+
+    /// Starts the `TweenManager`.  Available on platforms other than macOS.
+    @available(iOS 13, tvOS 13, macCatalyst 13, visionOS 1, *)
     public func start() async {
+        #if !os(macOS)
         displayLinkProxy = await DisplayLinkProxy()
         startPollingTimeIntervals()
+        #endif
     }
-    #endif
-
+    
     func register(tweenInstance: TweenInstance) {
         if !runningTweenInstances.contains(where: { $0 === tweenInstance }) {
             runningTweenInstances.append(tweenInstance)
